fa1982ae294bacab6daab1347d67d5459602d6a2
Rawgher <rdpender3@gmail.com>
2020-05-18 15:41:41 -0700

    getting article description and title to show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 class ArticlesController < ApplicationController
 
   def show
+    # @ makes the variable into an instance variable that will be available in the show view
+    @article = Article.find(params[:id])
   end
 
 end
