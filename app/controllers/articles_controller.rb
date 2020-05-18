6859d360b32002d5780499e85a7a5551637c5246
Rawgher <rdpender3@gmail.com>
2020-05-18 16:23:48 -0700

    added create functionality and a redirect for form submission

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,11 +10,16 @@ class ArticlesController < ApplicationController
   end
 
   def new
-
   end
 
   def create
-    render plain: params[:article]
+    # Have to whitelist articles by requiring the top level key article and permit the title and description
+    # to come in at submission to create the new article instance
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article.save
+    redirect_to article_path(@article)
+    # can also just use below code
+    # redirect_to article_path(@article)
   end
 
 end
