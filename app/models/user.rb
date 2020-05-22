77b640eb7c462d200b69e6301ba6ee734c654c80
Rawgher <rdpender3@gmail.com>
2020-05-22 16:36:35 -0700

    adding downcase code to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_many :articles
+  # Before anything is added to the db it sets the email to be all lowercase 
+  before_save { self.email = email.downcase }
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
  # All caps variable name = constant
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
