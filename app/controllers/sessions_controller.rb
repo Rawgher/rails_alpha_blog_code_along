ff25204860cdff5066f394e56a0e362e5abacd73
Rawgher <rdpender3@gmail.com>
2020-05-25 10:59:05 -0700

    adding log in and log out functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,24 @@ class SessionsController < ApplicationController
   end
 
   def create
-    
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      # allows user to authenticate here and then stay logged in until they use the destroy function (this is a cookie)
+      # other things can be saved in the session portion to be used in the application
+      session[:user_id] = user.id
+      flash[:notice] = "Logged in successfully"
+      redirect_to user
+    else
+      flash.now[:alert] = "There was something wrong with your login credentials"
+      render 'new'
+    end
   end
 
   def destroy
+    # clearing out the cookie stored from the create function to log the user out
+    session[:user_id] = nil
+    flash[:notice] = "Logged out"
+    redirect_to root_path
   end
 
 end
