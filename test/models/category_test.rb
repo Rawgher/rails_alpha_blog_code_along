e4b7a684a67b26ac9c3557f4ab4c40e11e6d2000
Rawgher <rdpender3@gmail.com>
2020-05-28 17:55:06 -0700

    adding more tests and a setup method

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,10 +1,33 @@
 require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
+  # each test gets scrubbed so need to do @ for each test
+
+  # this method helps to avoid repeat code
+  # it will run with each test
+  def setup
+    @category = Category.new(name: "Sports")
+  end
 
   test "category should be valid" do
-    @category = Category.new(name: "Sports")
     assert @category.valid?
+  end
+
+  test "name should be present" do
+    @category.name = " "
+    assert_not @category.valid?
+  end
+
+  test "name should be unique" do
+
+  end
+
+  test "name should not be too long" do
+
+  end
+
+  test "name shoudl not be too short" do
+
   end
 
 end
